d0105cec59fb1d75b0858be6de1adcba8190d20e
Elvis <79464171+DynastyElvis@users.noreply.github.com>
2025-03-10 21:34:23 +0300

    ft:provideHttpClient App configuration

--- angular-data-table/src/app/app.config.ts
+++ angular-data-table/src/app/app.config.ts
@@ -1,8 +1,21 @@
+// import { provideHttpClient } from '@angular/common/http';
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideRouter(routes)]
+// };
+
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http'; // Add this line
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(), // Add this line
+  ]
 };
