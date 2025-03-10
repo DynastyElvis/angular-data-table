7d667a4e2334268367f213d60285c7e4684787fa
Elvis <79464171+DynastyElvis@users.noreply.github.com>
2025-03-10 21:49:34 +0300

    bg: app bug fixes

--- angular-data-table/src/app/app.component.ts
+++ angular-data-table/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Add this line
+import { DataTableComponent } from './data-table/data-table.component'; // Add this line
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, // Add this line
+  imports: [CommonModule, DataTableComponent], // Add this line
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-data-table';
