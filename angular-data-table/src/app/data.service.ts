ed792762f4a97b8660b822bb998a0627e9c0ad1e
Elvis <79464171+DynastyElvis@users.noreply.github.com>
2025-03-10 21:36:12 +0300

    ft:data service logic

--- angular-data-table/src/app/data.service.ts
+++ angular-data-table/src/app/data.service.ts
@@ -1,9 +1,35 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DataService {
+
+//   constructor() { }
+// }
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users'; // Example API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      catchError(this.handleError<any[]>('getData', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console
+      return of(result as T);
+    };
+  }
 }
